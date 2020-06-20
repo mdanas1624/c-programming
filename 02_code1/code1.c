55937d34fa3bc23e71d20a8529d5df32363f1a21
Coursera Learner <nobody@nowhere.nul>
2020-06-20 16:56:59 +0000

    Did assignment 02_co
    clear
    cd ..

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -1,4 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 int max (int num1, int num2) {
+  if(num1>num2){
+    printf("%d",num1);
+    return num1;}
+  else{
+    printf("%d",num2);
+    return num2;}
   //check if num1 is greater than num2
     //if so, your answer is num1
     //otherwise, your answer is num2
